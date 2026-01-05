23c72c63a0534ead2460b46eadfd99d2d5a9d5de
GaryOcean <81794144+GaryOcean428@users.noreply.github.com>
2026-01-05 17:27:23 +0800

    feat: Export new route modules from index
    Added exports for autonomic, immune, training, memory, feedback, and coordizer routers

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -19,7 +19,23 @@ export { adminRouter } from "./admin";
 
 export { default as olympusRouter } from "./olympus";
 
-export { autonomicAgencyRouter } from "./autonomic-agency";
+// Autonomic routes (full kernel + agency)
+export { autonomicRouter, autonomicAgencyRouter } from "./autonomic";
+
+// Immune system routes
+export { immuneRouter } from "./immune";
+
+// Training routes
+export { trainingRouter } from "./training";
+
+// Memory routes
+export { memoryRouter } from "./memory";
+
+// Feedback routes
+export { feedbackRouter } from "./feedback";
+
+// Coordizer routes
+export { coordizerRouter } from "./coordizer";
 
 export { federationRouter } from "./federation";
 
